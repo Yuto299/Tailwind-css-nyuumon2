b93a4edbce312d1d77f590406301586a65384d05
Yuto299 <ota.se.ke@outlook.jp>
2025-02-15 01:32:29 +0900

    space-

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     // ナビゲーションバー
     <header className='py-6'>
-      <div className='container mx-auto'>
+      <div className='container mx-auto flex justify-between items-center px-8 md:px-14 lg:px-24 w-full'>
         <div>Yut.com</div>
         <div>
           <a href='#home'>ホーム</a>
