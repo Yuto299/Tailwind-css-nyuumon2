fc0a1bbc88cfa7f0282ea5cab543ee1c9fa920c2
Yuto299 <ota.se.ke@outlook.jp>
2025-02-15 01:21:42 +0900

    flex-

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
 import './App.css';
 
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    // ナビゲーションバー
+    <header className='py-6'>
+      <div className='container mx-auto'>
+        <div>Yut.com</div>
+        <div>
+          <a href='#home'>ホーム</a>
+          <a href='#portfolio'>ポートフォリオ</a>
+          <a href='#clients'>お客様</a>
+          <a href='#contact'>
+            <button>お問い合わせ</button>
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 }
